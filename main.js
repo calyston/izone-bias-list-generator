096123d420b663bb4311efb737453d567c0082c3
unknown <calyston.14@gmail.com>
2020-04-24 13:02:24 -0400

    feat: event listeners added to options

---
+++ main.js
@@ -0,0 +1,11 @@
+const selectedOption = document.getElementById('optionA')
+
+selectedOption.addEventListener('click', () => {
+  console.log("you clicked A")
+})
+
+const sortedOption = document.getElementById('optionB')
+
+sortedOption.addEventListener('click', () => {
+  console.log("you clicked B")
+})
