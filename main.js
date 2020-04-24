defb7cfefc55433e8513553f9c0f61cb5a1efc45
unknown <calyston.14@gmail.com>
2020-04-24 13:35:03 -0400

    feat: both arrays for sorting established

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ const sortedOption = document.getElementById('optionB')
 sortedOption.addEventListener('click', () => {
   console.log("you clicked B")
 })
+
+//Variables for the Options
+const izoneMembers = [
+  { name: '1', photo: 'assets/images/wonyoung.jpeg' },
+  { name: '2', photo: 'assets/images/sakura.jpeg' },
+  { name: '3', photo: 'assets/images/yuri.jpeg' },
+  { name: '4', photo: 'assets/images/yena.jpeg' }
+]
+
+const resultsArray = []
+
+Math.floor(Math.random() * izoneMembers.length)
+//outputs random index from the array
+
+if (resultsArray.length === 0) {
+  const randomIndex = Math.floor(Math.random() * izoneMembers.length)
+  const [initialMember] = izoneMembers.splice(randomIndex, 1)
+  console.log(initialMember)
+  resultsArray.push(initialMember)
+  console.log(resultsArray)
+}
+
